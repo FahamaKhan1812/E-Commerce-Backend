5dc0e3f5171e35564eabfa05e86c405238ea873e
fahama khan <fahamakhan990@gmail.com>
2023-06-04 23:15:38 +0500

    add new methods

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -5,4 +5,7 @@ public interface IProductRepository
 {
     Task<IReadOnlyList<Products>> GetProductsAsync();
     Task<Products> GetProductByIdAsync(int id);
+    Task<IReadOnlyList<ProductType>> GetProductTypessAsync();
+    Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+
 }
