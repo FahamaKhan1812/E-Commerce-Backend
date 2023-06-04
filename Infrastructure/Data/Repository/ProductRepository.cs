b833012162fccc1603e60b62add3a7261b7f4b5b
fahama khan <fahamakhan990@gmail.com>
2023-06-04 20:16:39 +0500

    add new class for repository

---
+++ Infrastructure/Data/Repository/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.Repository;
+public class ProductRepository : IProductRepository
+{
+    private readonly StoreContext _context;
+
+    public ProductRepository(StoreContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Products> GetProductByIdAsync(int id)
+    {   
+        return await _context.Products.FindAsync(id);
+    }
+
+    public async Task<IReadOnlyList<Products>> GetProductsAsync()
+    {
+        return await _context.Products.ToListAsync();
+    }
+}
