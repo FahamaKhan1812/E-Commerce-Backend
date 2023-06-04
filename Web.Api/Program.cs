acc8c02cb11f4661fbab440398c039b619e2daa0
fahama khan <fahamakhan990@gmail.com>
2023-06-04 20:14:56 +0500

    add service

--- Web.Api/Program.cs
+++ Web.Api/Program.cs
@@ -15,6 +15,9 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection"));
 });
 
+// Servies
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
