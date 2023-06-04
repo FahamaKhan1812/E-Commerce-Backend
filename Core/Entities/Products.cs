7c9f920886105f0bbb6a9d9ac450e51040fb8564
fahama khan <fahamakhan990@gmail.com>
2023-06-04 20:11:47 +0500

    add new properties

--- Core/Entities/Products.cs
+++ Core/Entities/Products.cs
@@ -1,6 +1,12 @@
 ﻿namespace Core.Entities;
 public class Products : BaseEntity
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal? Price { get; set; }
+    public string? PictureUrl { get; set; }
+    public ProductType? ProductType { get; set; }
+    public int ProductTypeId { get; set; }
+    public ProductBrand? ProductBrand { get; set; }
+    public int ProductBrandId { get; set; }
 }
