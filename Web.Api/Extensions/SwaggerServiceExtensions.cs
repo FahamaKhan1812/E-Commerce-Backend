1f411812ba8d544a254fe80339d911a46a3dfa94
fahama khan <fahamakhan990@gmail.com>
2023-07-29 19:44:12 +0500

    add swagger documention class but not working

---
+++ Web.Api/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Web.Api.Extensions;
+public static class SwaggerServiceExtensions
+{
+    public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "E-Commerece App", Version = "1" });
+        });
+        return services;
+    }
+    public static IApplicationBuilder UseSwaggerDocumention (this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        return app;
+    } 
+}
